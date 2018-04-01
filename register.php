cad1f9fc7867d657347b492c850c57d360107942
Arkadiusz Kina <arkadiuszkina@MacBook-Pro-Arkadiusz.local>
2018-04-01 19:34:27 +0200

    Added sample validator

--- register.php
+++ register.php
@@ -1,41 +1,38 @@
 <!DOCTYPE html>
 <html lang="pl">
-
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1" />
 	<link rel="stylesheet" href="styles/style.css" />
 	<script src="scripts/jquery-3.3.1.min.js"></script>
-	<script src="scripts/validate.js"></script> 	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script type="text/javascript" src="scripts/validator.js"></script>
 	<title>Rejestracja</title>
 </head>
 
 <body>
 	<header>
-		<?php
-				include 'modules/header.php';
-		?>
-		<nav id="topnav">
 			<?php
-				include 'modules/menu.php';
+				include_once 'modules/header.php';
 			?>
-		</nav>
+			<nav id="topnav">
+			<?php
+				include_once 'modules/menu.php';
+			?>
+			</nav>
 	</header>
 	<main>
-		<div id="logowanie">
-		<article>
-				<form method="post" id="rejestracja" action="register.php">
-					<input type="email" name="email" placeholder="email" onfocus="this.placeholder=''" onblur="this.placeholder='login'" />
-					<input type="password" id="password" name="password" placeholder="hasło" onfocus="this.placeholder=''" onblur="this.placeholder='hasło'" />
-					<input type="password" name="confirmPassword" placeholder="Powtórz hasło" onfocus="this.placeholder=''" onblur="this.placeholder='Powtórz hasło'" />
-					<input type="submit" value="Zaloguj się" />
-				</form>
-		</article>
+		<div class="register">
+			<form method="post" action="register.php">
+				<span class="popuptext" id="emailValidationPopup">Błędny adres email.</span>
+				<input type="text" id="emailInput" placeholder="Adres email"/>
+				<input type="password" id="passwordInput" placeholder="Hasło"/>
+				<input type="password" id="repeatPasswordInput" placeholder="Powtórz hasło"/>
+				<input type="submit" value="Zaloguj się" />
+			</form>
 		</div>
-	<?php
-		include 'modules/footer.php';
-	?>
+		<?php
+			include_once 'modules/footer.php';
+		?>
+	</main>
 </body>
-
 </html>
---
+++ scripts/validator.js
@@ -0,0 +1,24 @@
+function validateEmail(email) {
+    var expression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return expression.test(String(email).toLowerCase());
+}
+
+function validatePassword(password) {
+    var expression = /^[A-Za-z0-9_.]+$/;
+    return expression.test(String(password).toLowerCase());
+}
+
+$(document).ready(function() {
+    $('#emailInput').bind('input', function() {
+        var popup = document.getElementById("emailValidationPopup");
+        if(!validateEmail($(this).val())) {        
+            if(!$(popup).hasClass("show"))
+                popup.classList.toggle("show");
+        }
+        else{
+            if($(popup).hasClass("show"))
+                $(popup).removeClass("show");
+        }
+    });
+});
+
