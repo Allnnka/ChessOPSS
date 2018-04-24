9c35d2075bc2e8dfef67bb0a24acd13c93201265
Arkadiusz Kina <arkadiuszkina@MacBook-Pro-Arkadiusz.local>
2018-04-24 13:27:58 +0200

    Revert changes

--- scripts/validator.js
+++ scripts/validator.js
@@ -1,30 +1,56 @@
-$.getScript("validatorRules.js", function(){});
+function validateEmail(email){
+    var expression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return expression.test(String(email).toLowerCase());
+}
 
-$(document).ready(function() {
-    $('#emailInput').bind('input', function() {
-        var popup = document.getElementById("emailValidationPopup");
-        if(!validateEmail($(this).val())) {        
+function validatePassword(password){
+    var expression = /^\w{6,30}$/;
+    return expression.test(String(password).toLowerCase());
+}
+
+function bindValidationToInput(inputId, popupId, validationMethodName){
+    $(document).ready(function() {
+        $(inputId).bind("input", function() {
+            var popup = document.getElementById(popupId);
+            if(!validationMethodName($(this).val())) {        
+                if(!$(popup).hasClass("show"))
+                    popup.classList.toggle("show");
+            }
+            else{
+                if($(popup).hasClass("show"))
+                    $(popup).removeClass("show");
+            }
+        });
+    });
+}
+
+var validationInputs = ["#emailInput", "#passwordInput"];
+var validationPopups = ["emailValidationPopup", "passwordValidationPopup"];
+var validationMethods = [validateEmail, validatePassword];
+
+function isValidated(){
+    for(i = 0; i < validationInputs.length; i++){
+        if(!validationMethods[i]($(validationInputs[i]).val())){
+            var popup = document.getElementById(validationPopups[i]);
             if(!$(popup).hasClass("show"))
                 popup.classList.toggle("show");
+            return false;
         }
-        else{
-            if($(popup).hasClass("show"))
-                $(popup).removeClass("show");
+    }
+    return true;
+}
+
+$(document).ready(function() {
+    $('#registerForm').on('submit', function(e) {
+        if(!isValidated()){
+            e.preventDefault();
+            console.log("not valid.");
         }
     });
 });
 
-
 $(document).ready(function() {
-    $('#passwordInput').bind('input', function() {
-        var popup = document.getElementById("passwordValidationPopup");
-        if(!validatePassword($(this).val())) {        
-            if(!$(popup).hasClass("show"))
-                popup.classList.toggle("show");
-        }
-        else{
-            if($(popup).hasClass("show"))
-                $(popup).removeClass("show");
-        }
-    });
+    for(i = 0; i < validationInputs.length; i++){
+        bindValidationToInput(validationInputs[i], validationPopups[i], validationMethods[i]);
+    }
 });
